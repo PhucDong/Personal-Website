f1282de92a3366674661aa483f6f169bdee840a4
Phuc Dong <dongvuminhphuc@gmail.com>
2023-02-27 17:18:04 +0700

    Toggle navbar appear or hidden

--- scripts.js
+++ scripts.js
@@ -2,10 +2,5 @@ let hamburgerMenu = document.querySelector(".hamburger-menu");
 let navigationBar = document.querySelector(".navbar");
 
 hamburgerMenu.addEventListener("click", () => {
-    if (navigationBar.style.display === "block") {
-        navigationBar.style.display = "none";
-    } 
-    else {
-        navigationBar.style.display = "block";
-    }
+    navigationBar.classList.toggle("appear");
 });
